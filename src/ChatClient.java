d535de8b8653ed3900b9b7d09a6bf1f49a894594
liuqingtao <liuqingtao86@gmail.com>
2014-06-30 11:03:26 +0800

    Chat 2.0

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,36 +3,26 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 
-public class ChatClient {
+public class ChatClient extends Frame{
 
+	TextField tf = new TextField();
+	TextArea ta = new TextArea();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyFrame frame = new MyFrame();
+		new ChatClient().launchFrame();
 	}
-
-}
-
-class MyFrame extends Frame{
-	MyFrame(){
-		setLayout(null);
-		setBounds(400,300,300,500) ;
-		setVisible(true);
-		addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent e){
-				System.exit(0);				
-			}
-		});
-		Panel p1 = new Panel();
-		TextArea ta = new TextArea();
-		ta.setBackground(Color.WHITE);
-		p1.setBounds(0,0,300,300);
-		TextField tf = new TextField("9999231");
-		tf.setBounds(0,0,300,200);
+	
+	public void launchFrame(){
+		setLocation(400, 300);
+		setSize(300,400);
+		add(tf,BorderLayout.SOUTH);
+		add(ta,BorderLayout.NORTH);
 		tf.setBackground(Color.WHITE);
-		p1.add(ta);
-		p1.add(tf);
-		add(p1);
+		ta.setBackground(Color.WHITE);
+		pack();
+		setVisible(true);
 	}
-	
-	
+
 }
+
+
